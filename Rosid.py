0ab4e512dd2f8c9affb971162e3e558628d41be8
Rosid456 <126705533+Rosid456@users.noreply.github.com>
2023-03-10 01:12:22 +0600

    Create Rosid.py
    git clone https://github.com/Rosid456/rs.git
    ls
    cd ts
    ls
    Python Rosid.py

---
+++ Rosid.py
@@ -0,0 +1,8 @@
+import os,time
+os.system("clear")
+print("\n\033[38;5;195m[\033[38;5;46m+\033[38;5;195m]\033[38;5;46m CHECKING UPDATE 💚")
+time.sleep(2)
+os.system("git pull")
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://www.facebook.com/profile.php?id=100018760997097&mibextid=ZbWKwL
